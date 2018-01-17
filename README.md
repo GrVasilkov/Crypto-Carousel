aaeb4054fe3ae2026cb85b2ac249ff1d0be6b50a
Grigory Vasilkov <gr.vasilkov@gmail.com>
2018-01-17 17:37:54 +0300

    Crypto Carousel v0.1

--- README.md
+++ README.md
@@ -1,1 +1,109 @@
-# Crypto-Carousel
+This python script crawling the site coinmarketcap.com, obtains a list of existing crypto currency and then in each of them selects unique tradable pairs on exchanges and makes a comparison between items on which of them the difference in value is greater.
+
+Example output:
+[{
+	"Bitcoin": {
+		"Differnce": [
+			[{
+				"pair": "XDN/BTC",
+				"max diff": "27.2738809414 %",
+				"HitBTC": 17336.0,
+				"Bittrex": 12607.8
+			}],
+			[{
+				"pair": "EOS/BTC",
+				"Coinrail": 12751.2,
+				"Kraken": 16818.5,
+				"max diff": "31.8973900496 %"
+			}, {
+				"pair": "EOS/BTC",
+				"ZB.COM": 13420.4,
+				"Kraken": 16818.5,
+				"max diff": "25.3204077375 %"
+			}],
+			[{
+				"pair": "XLM/BTC",
+				"Binance": 13950.2,
+				"max diff": "30.5244704746 %",
+				"Kraken": 20079.3
+			}, {
+				"pair": "XLM/BTC",
+				"max diff": "29.9582156748 %",
+				"Kraken": 20079.3,
+				"Bittrex": 14063.9
+			}],
+			[{
+				"pair": "BTC/KRW",
+				"Coinrail": 14151.2,
+				"max diff": "29.0752727684 %",
+				"Bithumb": 18265.7
+			}, {
+				"pair": "BTC/KRW",
+				"Coinone": 18596.0,
+				"Coinrail": 14151.2,
+				"max diff": "31.4093504438 %"
+			}]
+		],
+		"Market Cap": "$237,241,768,665",
+		"Coin URL": "https://coinmarketcap.com/currencies/bitcoin/",
+		"Coin ID": "1",
+		"Price": "$14123.10",
+		"Change": "3.53%"
+	}
+}, {
+	"Ethereum": {
+		"Differnce": [
+			[{
+				"pair": "ETH/KRW",
+				"Coinrail": 1304.56,
+				"max diff": "26.6358005764 %",
+				"Bithumb": 1652.04
+			}, {
+				"pair": "ETH/KRW",
+				"Coinrail": 1304.56,
+				"GOPAX": 1657.21,
+				"max diff": "21.2797412519 %"
+			}],
+			[{
+				"pair": "ITC/ETH",
+				"Huobi": 1329.12,
+				"Bibox": 1103.35,
+				"max diff": "20.4622286672 %"
+			}],
+			[{
+				"pair": "EOS/ETH",
+				"max diff": "21.1222461151 %",
+				"Kraken": 1492.65,
+				"HitBTC": 1232.35
+			}, {
+				"pair": "EOS/ETH",
+				"Liqui": 1235.27,
+				"max diff": "20.8359306063 %",
+				"Kraken": 1492.65
+			}],
+			[{
+				"pair": "WAX/ETH",
+				"Tidex": 1705.72,
+				"Huobi": 1254.2,
+				"max diff": "26.4709330957 %"
+			}],
+			[{
+				"pair": "ETC/ETH",
+				"Bibox": 1467.66,
+				"Kraken": 1222.4,
+				"max diff": "20.0638089005 %"
+			}],
+			[{
+				"pair": "ETH/EUR",
+				"CoinFalcon": 1099.68,
+				"Exmo": 1360.8,
+				"max diff": "23.7450894806 %"
+			}]
+		],
+		"Market Cap": "$119,116,252,263",
+		"Coin URL": "https://coinmarketcap.com/currencies/ethereum/",
+		"Coin ID": "2",
+		"Price": "$1228.88",
+		"Change": "1.29%"
+	}
+}]
---
+++ scan.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+
+import requests
+import json
+import logging
+import lxml.html
+
+data_url = "https://coinmarketcap.com"
+percentage_difference = 20
+
+logging.basicConfig(format = u'%(levelname)-8s [%(asctime)s] %(message)s', level = logging.DEBUG, filename = u'carousel_log.log')
+
+def scan_cryptocoins():
+    page = requests.get(data_url).text
+    parser = lxml.html.fromstring(page)
+    coins_result = []
+    coins = parser.cssselect("div[class='container'] tbody tr")
+
+    for i in coins:
+        pair_array = []
+        diff_pair_forshow = []
+        coin_id = i.cssselect("tr td")[0].text.strip()
+        coin_name = i.cssselect("tr td a.currency-name-container")[0].text
+        coin_url = data_url+i.cssselect("tr td a.currency-name-container")[0].get("href")
+        coin_marketcap = i.cssselect("tr td")[2].text.strip()
+        coin_price = i.cssselect("tr td a.price")[0].text
+        coin_change = i.cssselect("tr td")[6].text
+        page2 = requests.get(coin_url).text
+        parser2 = lxml.html.fromstring(page2)
+    
+        exchanges = parser2.cssselect("div[class='container'] div.table-responsive tbody tr")
+
+        for p in exchanges:
+            pair = p.cssselect("tr td a")[1].text
+            pair_array.append(pair)
+
+        pair_uniq = set(pair_array)
+        
+#        print coin_name
+        for pu_item in pair_uniq: 
+            diff_exchange_pair_array = []
+            diff20_exchange_pair_array = []
+
+            for tb_row in exchanges: # make uniq pair {exchange: price}
+                if (pu_item == tb_row.cssselect("tr td a")[1].text):
+                    volume = tb_row.cssselect("tr td span")[0].get("data-usd")
+                    if (volume > 100000): 
+                        exchange_price = tb_row.cssselect("tr td span")[1].get("data-usd")
+                        exchange_source = tb_row.cssselect("tr td a")[0].text
+                        bunch = {exchange_source: exchange_price}
+                        diff_exchange_pair_array.append(bunch)
+
+            if (len(diff_exchange_pair_array) > 1):
+#                print diff_exchange_pair_array
+                for index, pair_price in enumerate(diff_exchange_pair_array):
+                    exchange_analyze1 = pair_price.keys()[0]
+                    price_analyze1 = float(pair_price.values()[0])
+
+                    for next_item_pair in diff_exchange_pair_array[index+1:]:
+                        exchange_analyze2 = next_item_pair.keys()[0]
+                        price_analyze2 = float(next_item_pair.values()[0])
+                        percentage_price = 100-(price_analyze1*100/price_analyze2)
+                        if (percentage_price <0):
+                            percentage_price = percentage_price*(-1)
+
+#                        print percentage_price
+                        if (percentage_difference <= percentage_price):
+                            bunch = {"pair": pu_item,exchange_analyze1: price_analyze1, exchange_analyze2: price_analyze2 , "max diff": str(percentage_price)+" %"}
+#                            print bunch
+                            diff20_exchange_pair_array.append(bunch)
+
+                if (len(diff20_exchange_pair_array)> 0):
+                    diff_pair_forshow.append(diff20_exchange_pair_array)
+        
+        if (len(diff_pair_forshow)> 0):
+            coin_result = { 
+                    coin_name: {
+                                "Coin ID": coin_id,
+                                "Coin URL": coin_url,    
+                                "Market Cap": coin_marketcap,
+                                "Price": coin_price,
+                                "Change": coin_change,
+                                "Differnce": diff_pair_forshow                
+                                } 
+                    }
+
+            coins_result.append(coin_result)
+#        print json.dumps(coin_result)
+
+    print json.dumps(coins_result)
+
+
+
+scan_cryptocoins()
+
